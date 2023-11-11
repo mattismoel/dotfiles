eeb23fffec7eeeaa3d56da109ce34cbba55f9fba
Mattis Kristensen <Mattis Ml Kristensen>
2023-11-11 13:32:50 +0100

    cleanup init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 require("keymap")
 require("defaults")
 
+-- Setup Lazy Nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -15,6 +16,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
+
 
 -- COLORSCHEME
 vim.cmd.colorscheme "catppuccin"

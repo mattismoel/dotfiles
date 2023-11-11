891f48da32f74b380d9f49b4e1564c620e5f4ce4
Mattis Kristensen <Mattis Ml Kristensen>
2023-11-11 13:32:11 +0100

    update nvim-cmp confirm handling

--- nvim/after/plugin/nvim-cmp.lua
+++ nvim/after/plugin/nvim-cmp.lua
@@ -7,7 +7,7 @@ cmp.setup({
         end,
     },
     mapping = cmp.mapping.preset.insert({
-        ["<Tab>"] = cmp.mapping.confirm(),
+        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
     }),
     sources = cmp.config.sources({
         { name = "nvim_lsp" },

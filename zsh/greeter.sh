f3febdeb5d2ebb1664ad3e99c9c6acfc58034fe2
Mattis Møl Kristensen <mattismoel@gmail.com>
2024-01-06 20:51:21 +0100

    updated greeter

--- zsh/greeter.sh
+++ zsh/greeter.sh
@@ -1,6 +1,12 @@
 echo "Welcome back, Mattis!"
 
 if command -v fortune &> /dev/null && command -v cowsay &> /dev/null; then
-  fortune | cowsay
+  randomfortune=$(fortune)
+
+  while [[ ${#fortune} > 1 ]]; do
+    $randomfortune=$(fortune)
+  done
+
+  echo $randomfortune | cowsay
   echo
 fi

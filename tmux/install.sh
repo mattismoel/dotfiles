d97ad568308d96dd8c32513446cd8d24a3eb4036
Mattis Møl Kristensen <mattismoel@gmail.com>
2024-01-07 12:20:04 +0100

    fixed an issue where .zshrc sourced the install script by mistake

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,13 +1,22 @@
 source "$DOT/helpers.sh"
 source "$DOT/colors.sh"
 
-if [ -f "$HOME/.tmux.conf" ] || [ -L "$HOME/.tmux.conf" ]; then
-  echo -e "${RED}warning:${NC} a .tmux.conf file already exists..."
-  if boolask "would you like to override? a backup will be placed with name .tmux.conf.backup"; then
-    mv "$HOME/.tmux.conf" "$HOME/.tmux.conf.backup"
+if [ -d "$HOME/.config/tmux" ]; then
+  echo -e "${RED}warning:${NC} a tmux configuration folder already exists..."
+  if boolask "would you like to override? a backup will be placed at ~/.config/tmux.backup/"; then
+    mv -r "$HOME/.config/tmux/" "$HOME/.config/tmux.backup/"
   fi
 fi
 
-ln -s "$WD/tmux/.tmux.conf" "$HOME/.tmux.conf"
+# if [ -f "$HOME/.tmux.conf" ] || [ -L "$HOME/.tmux.conf" ]; then
+#   echo -e "${RED}warning:${NC} a .tmux.conf file already exists..."
+#   if boolask "would you like to override? a backup will be placed with name .tmux.conf.backup"; then
+#     mv "$HOME/.tmux.conf" "$HOME/.tmux.conf.backup"
+#   fi
+# fi
+#
+#
+
+ln -s "$DOT/tmux/" "$HOME/.config/tmux"
 source "./tmux/plugins.sh"
 echo -e "${GREEN}completed set up of tmux${NC}"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,10 +1,15 @@
 export ZSHPATH="$HOME/.config/zsh"
 export DOT="$HOME/.dotfiles"
 
+# Loops over each file in zsh config directory and sources them.
+# Makes sure that the "install.sh" is not sourced,
+# as that is done by the main dotfile "$DOT/install.sh" script.
 if [ -d $ZSHPATH ]; then
   for file in $ZSHPATH/*.sh; do
+    if [ "$(basename $file)" = "install.sh" ]; then
+      continue
+    fi
     source $file
   done
 fi
 
-# source $ZSHPATH/aliases.zsh

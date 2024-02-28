14327cc9c3998992f2d0c558716edfc83f69093a
Mattis Møl Kristensen <mattismoel@gmail.com>
2024-02-28 17:38:08 +0100

    add templ

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -20,8 +20,10 @@ return {
         lua_ls = {},
         bashls = {},
         clangd = {},
+        templ = {},
       }
 
+      vim.filetype.add({ extension = { templ = "templ" } })
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 

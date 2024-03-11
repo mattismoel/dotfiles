8513fdbcd4da5797c2409feea24afb0d4a5b750e
Mattis Møl Kristensen <mattismoel@gmail.com>
2024-03-11 21:04:33 +0100

    update vimtex defaults

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,9 +1,9 @@
 return {
   "lervag/vimtex",
   init = function()
-    vim.g.vimtext_view_method = "zathura"
-    vim.g.vimtex_view_general_viewer = "okular"
-    vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
+    vim.g.vimtex_quickfix_enabled = 0
+    -- vim.g.vimtex_view_general_viewer = "okular"
+    -- vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
     vim.g.vimtex_compiler_method = "latexmk"
   end,
   ft = { "tex" },

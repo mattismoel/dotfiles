f2f24db37c7a2a735bda31e754ee322a9f0a0550
Mattis Møl Kristensen <mattismoel@gmail.com>
2024-04-30 23:33:06 +0200

    update lua line configuration

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,5 +2,10 @@ return {
   "nvim-lualine/lualine.nvim",
   opts = {
     theme = "dracula",
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff" },
+      lualine_c = { "diagnostics", "filename" }
+    }
   }
 }

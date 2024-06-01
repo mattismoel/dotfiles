2ecac67f81351ba470a647a6a8884354a45d8391
Mattis Kristensen <mattismoel@gmail.com>
2024-06-01 14:15:08 +0200

    rename zsh alias to be example

---
+++ zsh/.aliases.example.sh
@@ -0,0 +1,3 @@
+# Aliases
+alias ls='ls --color' # Colored ls command.
+alias dot="cd $HOME/.config/dotfiles && nvim ."
--- zsh/.aliases.sh
+++
@@ -1,3 +0,0 @@
-# Aliases
-alias ls='ls --color' # Colored ls command.
-alias p="cd ~/Documents/Programmering/Projekter && clear"

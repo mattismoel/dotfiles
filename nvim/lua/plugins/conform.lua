28c9c47249ba1f73c482c3f37382767722aed348
Mattis Kristensen <mattismoel@gmail.com>
2025-11-16 11:50:35 +0100

    use prettier formatter for conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,8 +2,12 @@ return {
   'stevearc/conform.nvim',
   opts = {
     format_on_save = {
-      timeout_ms = 500,
+      timeout_ms = 1000,
       lsp_format = "fallback",
-    }
+    },
+		formatters_by_ft = {
+			javascript = {  "prettier", stop_after_first = true },
+			svelte = {  "prettier", stop_after_first = true }
+		},
   },
 }
